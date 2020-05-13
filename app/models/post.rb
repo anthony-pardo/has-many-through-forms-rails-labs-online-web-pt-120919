fafbb757919df7b9bee8162980afd7dfcd779971
Anthony <pardo.anthony@gmail.com>
2020-05-13 16:29:41 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,17 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :comments, reject_if: :any_blank
 
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category if category.name != ""
+    end
+  end
+
+  def any_blank(attributes)
+    attributes['content'].blank? || attributes['user_id'].blank?
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(username: "user1")
+User.create(username: "user2")
+User.create(username: "user3")
+User.create(username: "user4")
+User.create(username: "user5")
+
+category = Category.create(name: "Cool")
+post = Post.create(title: "So Awesome", content: "Man am I a great blogger OR WHAT!")
+user = User.create(username: 'RealBigFish')
